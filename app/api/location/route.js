234024e47cf1f67cb113549925dd0b5a04c23720
sagar sangwan <iamsagarsangwan@gmail.com>
2024-11-29 20:57:13 +0530

    added autocomplete

---
+++ app/api/location/route.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+export async function POST(request) {
+    try {
+        const { q } = await request.json()
+        console.log(q, "---------------------------------------------------------------------------")
+
+        if (!q || q.length < 3) {
+            const res = JSON.stringify({ error: "search at least 3 word" })
+            return Response.json({ res })
+        }
+        console.log(q, "-------------------------------------")
+        const response = await axios.get("https://us1.locationiq.com/v1/autocomplete.php",
+            {
+                params: {
+                    key: process.env.LOCATIONIQ_API_KEY, // Secure token
+                    q,
+                    limit: 5,
+                }
+            }
+        )
+
+        console.log("Response Status:", response.status);
+        console.log("Response Data:", response.data);
+        return new Response(JSON.stringify(response.data), { status: 200 });
+    } catch (error) {
+        console.error("Error fetching autocomplete data:", error);
+        return new Response(
+            JSON.stringify({ error: "Failed to fetch autocomplete data" }),
+            { status: 500 }
+        );
+    }
+}
