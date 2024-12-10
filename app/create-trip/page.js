88f268c0698d4e0229f213454d3d97d7eab6697e
sagar sangwan <iamsagarsangwan@gmail.com>
2024-12-10 13:23:41 +0530

    added value in reach form

--- app/create-trip/page.js
+++ app/create-trip/page.js
@@ -2,18 +2,34 @@
 import { useState, useRef } from "react"
 import { z } from "zod"
 
-import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-// import { Span } from "next/dist/trace"
 
 
 const FormSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
+    destination: z.string().min(2, {
+        message: "destination must be at least 2 characters.",
     }),
 })
 function Page() {
+    const form = useForm({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            destination: "",
+        },
+    })
     const [query, setQuery] = useState("")
     const [suggestions, setSuggestions] = useState([])
     const [selectedPlace, setSelectedPlace] = useState("")
@@ -69,49 +85,48 @@ function Page() {
     const handleSuggestionClick = (place) => {
         setSelectedPlace(place.display_name)
         setQuery(place.display_name)
+        form.setValue("destination", place.display_name)
         setSuggestions([])
+    }
+    function onSubmit(values) {
+        console.log(values)
     }
 
 
     return (
         <div>
-            {/* <Form {...form}>
+            <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
                     <FormField
                         control={form.control}
-                        name="username"
+                        name="destination"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl> */}
-            <Input placeholder="search your place" value={query} onChange={handleInputChange} />
-            {apiError.length > 0 && (
-                <span>{apiError}</span>
-            )}
-            {suggestions.length > 0 && (
-                <ul>
-                    {suggestions.map((place) => (
+                                <FormLabel>destination</FormLabel>
+                                <FormControl>
+                                    <div>
+                                        <Input placeholder="search your place" {...field} value={query} onChange={handleInputChange} />
+                                        {apiError.length > 0 && (
+                                            <span>{apiError}</span>
+                                        )}
+                                        {suggestions.length > 0 && (
+                                            <ul>
+                                                {suggestions.map((place) => (
 
-                        <li key={place.place_id} onClick={() => handleSuggestionClick(place)}>{place.display_name}</li>
-                    ))}
-                </ul>
-            )}
-            {selectedPlace && (
-                <p className="mt-4">
-                    <strong>Selected Place:</strong> {selectedPlace}
-                </p>
-            )}
-            {/* </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
+                                                    <li key={place.place_id} onClick={() => handleSuggestionClick(place)}>{place.display_name}</li>
+                                                ))}
+                                            </ul>
+                                        )}
+
+                                    </div>
+                                </FormControl>
                                 <FormMessage />
                             </FormItem>
                         )}
                     />
                     <Button type="submit">Submit</Button>
                 </form>
-            </Form> */}
+            </Form>
         </div>
     )
 }
