277f825b54af6d55ea81efbeb73143295d4cbd30
sagar sangwan <iamsagarsangwan@gmail.com>
2024-12-11 18:26:33 +0530

    added shadcn date picker

--- components/trip/trip-input-steps.jsx
+++ components/trip/trip-input-steps.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { enUS } from "date-fns/locale";
+import { CalendarIcon } from "lucide-react"
 
 import {
     Form,
@@ -66,27 +67,87 @@ export const StepTwo = () => (
         <FormField
             name="startDate"
             render={({ field }) => (
-                <FormItem>
-                    <label>Start Date</label>
-                    <FormControl>
-                        <Input type="date" {...field} min={new Date().toISOString().split("T")[0]} />
-                    </FormControl>
+                <FormItem className="flex flex-col">
+                    <FormLabel>Date of birth</FormLabel>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <FormControl>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[240px] pl-3 text-left font-normal",
+                                        !field.value && "text-muted-foreground"
+                                    )}
+                                >
+                                    {field.value ? (
+                                        format(field.value, "PPP")
+                                    ) : (
+                                        <span>Pick a date</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                            </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                    date < new Date() || date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                            />
+                        </PopoverContent>
+                    </Popover>
+
                     <FormMessage />
                 </FormItem>
             )}
         />
+
         <FormField
             name="endDate"
             render={({ field }) => (
-                <FormItem>
-                    <label>End Date</label>
-                    <FormControl>
-                        <Input type="date" {...field} min={new Date().toISOString().split("T")[0]} />
-                    </FormControl>
+                <FormItem className="flex flex-col">
+                    <FormLabel>Date of birth</FormLabel>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <FormControl>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[240px] pl-3 text-left font-normal",
+                                        !field.value && "text-muted-foreground"
+                                    )}
+                                >
+                                    {field.value ? (
+                                        format(field.value, "PPP")
+                                    ) : (
+                                        <span>Pick a date</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                            </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                    date < new Date() || date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                            />
+                        </PopoverContent>
+                    </Popover>
+
                     <FormMessage />
                 </FormItem>
             )}
         />
+
     </>
 )
 export const StepThree = () => (
