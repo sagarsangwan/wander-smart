ccb8384c0b345fdb1d90f7611a9b5003127bd650
sagar sangwan <iamsagarsangwan@gmail.com>
2024-12-17 23:41:29 +0530

    ?

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,8 @@ import Providers from "@/components/providers";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 const inter = Inter({ subsets: ["latin"] });
+import Script from "next/script";
 
-// export const metadata = {
-//   title: "Wander Smart",
-//   description: "Unleash your travel potential with our intelligent itinerary planner",
-// };
 export const metadata = {
   title: "Wander Smart",
   description: "Unleash your travel potential with our intelligent itinerary planner",
@@ -19,7 +16,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "https://wander-smart.vercel.app/heroimage.svg", // Path to your image
+        url: "https://wander-smart.vercel.app/heroImage.png", // Path to your image
         width: 1200,
         height: 630,
         alt: "Preview of wander smart",
@@ -30,7 +27,7 @@ export const metadata = {
     card: "summary_large_image", // Use "summary" for a smaller card
     title: "Wander Smart",
     description: "Unleash your travel potential with our intelligent itinerary planner",
-    images: ["https://wander-smart.vercel.app/heroimage.svg"],
+    images: ["https://wander-smart.vercel.app/heroImage.png"],
   },
 };
 
@@ -38,6 +35,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
+        {process.env.NODE_ENV === 'production' && (
+          <Script id="clarity_script_script" strategy="afterInteractive">
+            {` 
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY}");
+
+            
+          `}
+          </Script>)}
         <Providers>
           <Navbar />
           <main className=" container">
