ca37fe4a633a643d7fb9fd467de9a4f7ba7a723a
sagar sangwan <iamsagarsangwan@gmail.com>
2024-12-20 00:26:28 +0530

    remove all extra code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://www.yourdomain.com', // Your site's domain URL
+    siteUrl: 'https://wander-smart.vercel.app', // Your site's domain URL
     generateRobotsTxt: true, // Automatically generate robots.txt
     changefreq: 'daily', // Frequency of updates
     priority: 0.7, // Priority of the pages
