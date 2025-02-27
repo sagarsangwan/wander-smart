8a3839262b57205c8fef13bef78caf4f0fad24d0
sagar sangwan <iamsagarsangwan@gmail.com>
2025-02-27 18:13:55 +0530

    removedectra code

--- components/trip/planExpired.jsx
+++ components/trip/planExpired.jsx
@@ -1,34 +1,25 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import Link from 'next/link'
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 function PlanExpired() {
-    return (
-        <div className='h-screen flex'>
-            <div className=' m-auto'>
-                <Card className="justify-center text-center">
-                    <CardHeader>
-                        <CardTitle>OOps, You are out of Token</CardTitle>
-                    </CardHeader>
-                    <CardFooter className="text-center justify-center ">
-                        <Button size="lg" className="animate-bounce">
-                            <Link href="/pricing">Buy Some Token</Link>
-                        </Button>
-                    </CardFooter>
-                </Card>
-
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-screen flex">
+      <div className=" m-auto">
+        <Card className="justify-center text-center">
+          <CardHeader>
+            <CardTitle>OOps, You are out of Token</CardTitle>
+          </CardHeader>
+          <CardFooter className="text-center justify-center ">
+            <Button size="lg" className="animate-bounce">
+              <Link href="/pricing">Buy Some Token</Link>
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
-export default PlanExpired
+export default PlanExpired;
